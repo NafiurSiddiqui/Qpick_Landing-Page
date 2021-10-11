3c3d70d03a61f903abb0a77723beb73e2b5953e4
TheHunter808 <saejim91@gmail.com>
2021-10-12 05:17:35 +0600

    interactive

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+const menubarContainer = document.querySelector('.header__menu-container');
+const menuContainer = document.querySelector('.header__menu__li-container');
+const menubarTop = document.querySelector('.header__menubars_top');
+const menubarMiddle= document.querySelector('.header__menubars_middle');
+const menubarBottom = document.querySelector('.header__menubars_bottom');
+const header = document.getElementsByTagName("header");
+console.log(header);
+
+
+// menubarContainer.addEventListener('click',()=>{
+//     header.className.toggle('open')
+// })
