34820e724652d5e5d2f4713bbb69eafffe984506
Nafi R <saejim91@gmail.com>
2022-12-12 19:15:36 +0600

    navbar bug fixed

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,19 +1,19 @@
 const menubarContainer = document.querySelector('.header__menu-container');
 const menuContainer = document.querySelector('.header__menu__li-container');
 const menubarTop = document.querySelector('.header__menubars_top');
-const menubarMiddle= document.querySelector('.header__menubars_middle');
+const menubarMiddle = document.querySelector('.header__menubars_middle');
 const menubarBottom = document.querySelector('.header__menubars_bottom');
 const header = document.querySelector('.header');
 
-
 let openMenu = false;
+let windowSize = window.innerWidth < 680;
 
-menubarContainer.addEventListener('click',()=>{
-   if(!openMenu){
-     header.classList.add('open')
-     openMenu = true;
-   }else{
-       header.classList.remove('open');
-       openMenu = false;
-   }
-})
+menubarContainer.addEventListener('click', () => {
+	if (!openMenu && windowSize) {
+		header.classList.add('open');
+		openMenu = true;
+	} else {
+		header.classList.remove('open');
+		openMenu = false;
+	}
+});
